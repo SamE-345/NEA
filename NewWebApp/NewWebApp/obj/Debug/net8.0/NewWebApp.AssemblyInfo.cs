3739682d41b0c5af5cc78450c655faf3723257e4
SamE-345 <sameveritt66@gmail.com>
2025-01-20 11:17:56 +0000

    Navbar is working with some links

--- NewWebApp/NewWebApp/obj/Debug/net8.0/NewWebApp.AssemblyInfo.cs
+++ NewWebApp/NewWebApp/obj/Debug/net8.0/NewWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97f64e174206c428d1f3acf3039b172ed8625cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c79f9bc1cddc4e365f644c630b4a50e139e67c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
