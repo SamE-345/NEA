017500b53de8b0dace158aa7a9cd40fa2e7d3a08
SamE-345 <sameveritt66@gmail.com>
2025-01-27 13:16:37 +0000

    Still working on DB connection

--- NewFrontend/NewFrontend/Form1.Designer.cs
+++ NewFrontend/NewFrontend/Form1.Designer.cs
@@ -28,21 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.UName_Input = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // UName_Input
+            // 
+            this.UName_Input.Location = new System.Drawing.Point(337, 53);
+            this.UName_Input.Name = "UName_Input";
+            this.UName_Input.Size = new System.Drawing.Size(100, 26);
+            this.UName_Input.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UName_Input);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox UName_Input;
     }
 }
 
--- NewFrontend/NewFrontend/Form1.cs
+++ NewFrontend/NewFrontend/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 
 namespace NewFrontend
 {
@@ -20,7 +20,42 @@ namespace NewFrontend
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            if(!Signin())
+            {
+                MessageBox.Show("Invalid Username or Password");
+            }
+
         }
+        private bool Signin()
+        {
+            string searchterm = UName_Input.Text;
+            string connection_string = "server=localhost;database=Database1";
+            MySqlConnection connection = new MySqlConnection(connection_string);
+            connection.Open();
+            string commandstring = "%" + searchterm + "%";
+            MySqlCommand command = new MySqlCommand();
+            command.CommandText = "SELECT Password FROM SignIn WHERE Name = @Search";
+            command.Parameters.AddWithValue("@Search", commandstring);
+            command.Connection = connection;
+
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string password = reader.GetString(0);
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        connection.Close();
+                        return true;
+                        // redirect to different form
+                    }
+                }
+
+            }
+            connection.Close();
+            return false;
+
+        }
+            
     }
 }
