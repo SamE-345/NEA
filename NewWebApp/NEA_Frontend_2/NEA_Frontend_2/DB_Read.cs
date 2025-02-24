15b491521612a3885a456229634268a34b58287e
SamE-345 <sameveritt66@gmail.com>
2025-02-24 13:21:35 +0000

    Added Database writing

--- NewWebApp/NEA_Frontend_2/NEA_Frontend_2/DB_Read.cs
+++ NewWebApp/NEA_Frontend_2/NEA_Frontend_2/DB_Read.cs
@@ -80,5 +80,36 @@ namespace NEA_Frontend_2
             return "message";
         }
     }
-    
+    public class DB_Write : Database_Modify
+    {
+        public DB_Write(string p_Username)
+        {
+            // Creates connection to DB
+            _connection.ConnectionString = Properties.Settings.Default.ChatDBConnectionString;
+            _Username = p_Username;
+        }
+        public void Add_Account(string Password)
+        {
+            _connection.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "INSERT INTO SignIn (Username, Password) VALUES (@Username, @Password)";
+            command.Parameters.AddWithValue("@Username", _Username);
+            command.Parameters.AddWithValue("@Password", _encrypt.Hash(Password));
+            command.Connection = _connection;
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+        public void Write_Message(string Message)
+        {
+            _connection.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "INSERT INTO Messages (Username, Message) VALUES (@Username, @Message)";
+            command.Parameters.AddWithValue("@Username", _Username);
+            command.Parameters.AddWithValue("@Message", Message);
+            command.Connection = _connection;
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+    }
+
 }
